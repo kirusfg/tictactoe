5c0c03c9342fa027510ed8d6f2091d212c4067b5
Kirill Kirillov <kirusfg@gmail.com>
2023-02-25 22:57:57 +0600

    Create a task to implement the game state

--- src/state.cpp
+++ src/state.cpp
@@ -1,87 +1,23 @@
-#include <algorithm>
-
 #include "state.h"
 
-State::State() : field(9), turn(Value::cross) {}
+State::State() {}
 
-std::vector<std::optional<Value>> State::getField() const { return field; }
+std::vector<std::optional<Value>> State::getField() const {}
 
-bool State::empty(size_t row, size_t col) {
-  return !field[row * 3 + col].has_value();
-}
+bool State::empty(size_t row, size_t col) {}
 
-std::optional<Move> State::generateRandom() {
-  if (!hasEmptyCells())
-    return std::nullopt;
+std::optional<Move> State::generateRandom() {}
 
-  size_t row = rand() % 3;
-  size_t col = rand() % 3;
+void State::make(Move m) {}
 
-  while (!empty(row, col)) {
-    row = rand() % 3;
-    col = rand() % 3;
-  }
+bool State::ended() {}
 
-  return Move(row, col, turn);
-}
+bool State::hasEmptyCells() {}
 
-void State::make(Move m) {
-  if (!empty(m.row, m.col))
-    return;
-  if (m.row > 2 || m.col > 2)
-    return;
+std::optional<Value> State::winner() {}
 
-  field[m.row * 3 + m.col] = m.value;
+Value State::getTurn() const {}
 
-  rotateTurn();
-}
+void State::rotateTurn() {}
 
-bool State::ended() {
-  if (!hasEmptyCells())
-    return true;
-  if (winner() != std::nullopt)
-    return true;
-
-  return false;
-}
-
-bool State::hasEmptyCells() {
-  return std::find(field.begin(), field.end(), std::nullopt) != field.end();
-}
-
-std::optional<Value> State::winner() {
-  for (size_t i = 0; i < 3; i++)
-    if (field[i * 3 + 0] == field[i * 3 + 1] &&
-        field[i * 3 + 1] == field[i * 3 + 2])
-      return field[i * 3 + 0];
-
-  for (size_t i = 0; i < 3; i++)
-    if (field[0 * 3 + i] == field[1 * 3 + i] &&
-        field[1 * 3 + i] == field[2 * 3 + i])
-      return field[0 * 3 + i];
-
-  if (field[0] == field[4] && field[4] == field[8])
-    return field[0];
-  if (field[2] == field[4] && field[4] == field[6])
-    return field[2];
-
-  return std::nullopt;
-}
-
-Value State::getTurn() const { return turn; }
-
-void State::rotateTurn() {
-  turn = turn == Value::circle ? Value::cross : Value::circle;
-}
-
-void State::print(std::ostream &out) const {
-  for (size_t i = 0; i < 3; i++) {
-    for (size_t j = 0; j < 3; j++) {
-      if (field[i * 3 + j].has_value())
-        out << field[i * 3 + j].value();
-      else
-        out << " ";
-    }
-    out << std::endl;
-  }
-}
+void State::print(std::ostream &out) const {}
--- src/state.h
+++ src/state.h
@@ -56,24 +56,45 @@ class State {
   std::vector<std::optional<Value>> field;
   Value turn;
 
+  // Simply passes the turn to the next player (from circle to cross, from cross
+  // to circle)
   void rotateTurn();
 
 public:
+  // Should initialize an empty field
   State();
 
+  // We do not want to manipulate the field directly, hence it is priivate
+  // However, in order to draw it in the Game class, we need to traverse it
   std::vector<std::optional<Value>> getField() const;
 
+  // Checks if the given cell is empty
+  // Rows and columns are counted from top-left
   bool empty(size_t row, size_t col);
+
+  // Generates a random move for the computer to make, very similar to what you
+  // have done in lab 5
   std::optional<Move> generateRandom();
 
+  // Applies the move on our game state
   void make(Move m);
 
+  // Checks if the game has ended (what conditions are there?)
   bool ended();
+
+  // Checks whether the field has any empty cells or not
   bool hasEmptyCells();
+
+  // Figures out the winner of the game if there is one
   std::optional<Value> winner();
 
+  // We do not want to manage the turn directly, either, that is why we made the
+  // field private. The only inconvenience is that we need a getter now.
+  // Do not create getters and setters for each and every of your class fields,
+  // though. This practice is a menace.
   Value getTurn() const;
 
+  // Do whatever you want
   void print(std::ostream &out) const;
 };
 
